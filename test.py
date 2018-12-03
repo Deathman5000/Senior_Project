593e76e525ce2fab4ec5f01b1f56d24d36b3f3ff
James Hund <jimmyhund@hotmail.com>
2018-12-03 11:21:30 -0600

    Here issome test files to mess with.

--- test.py
+++ test.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python
 import openpyxl
+import os
+import time
 
-sample=openpyxl.load_workbook('crack.xlsx') 
+begin = time.ctime()
+sample=openpyxl.load_workbook('testfile.xlsx')
 
 sheet = sample.active
+m_row = sheet.max_row
 
-a1 = sheet['A1']
-a2 = sheet['A2']
-a3 = sheet.cell(row=3, column=1)
+#a1 = sheet('A1')
+for i in range(1,m_row + 1):
+    cell = sheet.cell(row = i, column = 2)
+    print(cell.value)
+print "********************************************************************"
+for i in range(1,m_row + 1):
+    cell = sheet.cell(row = i, column = 3)
+    print(cell.value)
+#a1 = sheet['A1']
+#a2 = sheet['A2']
+#a3 = sheet.cell(row=3, column=1)
 
-print(a1.value)
-print(a2.value) 
-print(a3.value)
+#print(a1.value)
+#print(a2.value)
+#print(a3.value)
+#print a1.value
+end = time.ctime()
+print(begin)
+print(end)

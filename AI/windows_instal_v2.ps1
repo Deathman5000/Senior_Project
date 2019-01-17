c0c58a902f0ac0375c20401a16c8f404ff9d88a3
James Hund <jimmywhund@gmail.com>
2019-01-17 11:31:57 -0800

    Updated the file.

--- AI/windows_instal_v2.ps1
+++ AI/windows_instal_v2.ps1
@@ -4,18 +4,20 @@
 ################################################################################
 
 # install python
-Set-Location "C:\Users\James\Documents\CS4391\Senior_Project\AI"
-Start-Process 'C:\Users\James\Documents\CS4391\Senior_Project\AI\python-3.7.2.exe' -NoNewWindow -Wait
-$name = "$env:UserName"
-$PythonPath = "C:\Users\$name\AppData\Local\Programs\Python\Python37-32"
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -y
+choco install python pip
+#Set-Location "C:\Users\James\Documents\CS4391\Senior_Project\AI"
+#Start-Process 'C:\Users\James\Documents\CS4391\Senior_Project\AI\python-3.7.2.exe' -NoNewWindow -Wait
+#$name = "$env:UserName"
+#$PythonPath = "C:\Users\$name\AppData\Local\Programs\Python\Python37-32"
 [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";$PythonPath")
 #$PythonPath = gci C:\ -recurse -include python.exe -erroraction SilentlyContinue | select -Expand Directory -First 1 | select -Expand FullName
 # [System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";$PythonPath", "user")
 #python-3.7.2.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 # install pip.
-python get-pip.py
-$PipPath = "C:\Users\$name\AppData\Local\Programs\Python\Python37-32\Scripts"
-[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";$PipPath")
+#python get-pip.py
+#$PipPath = "C:\Users\$name\AppData\Local\Programs\Python\Python37-32\Scripts"
+#[System.Environment]::SetEnvironmentVariable("PATH", $Env:Path + ";$PipPath")
 # install openpyxl
 pip install openpyxl
 

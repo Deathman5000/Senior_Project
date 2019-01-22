41140ce25433f3d9d69b4d5e65508e7c1db7516e
Nathan Bradley <NJBradley1@buffs.wtamu.edu>
2019-01-22 15:53:31 -0600

    Made a clickable header

--- UI/UI.py
+++ UI/UI.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python
 
 import tkinter
+#from tkinter import LEFT, RIGHT
+from tkinter import FLAT
 
-WIDTH = 312
-HEIGHT = 324
+WIDTH = 350
+HEIGHT = 460
+TITLE_COLOR = "grey70"
 
 def nothing():
 	True
 
 main_window = tkinter.Tk()
+
 # Code to add widgets will go here...
 main_window.title( "Test UI for AI" )
-main_window.geometry("312x324") # size of the window width:- 500, height:- 375
-main_window.resizable(0, 0) # this prevents from resizing the window
-#main_window.width( 1000 )
-#main_window.height( 600 )
+main_window.geometry( '{}x{}'.format( WIDTH, HEIGHT) )
+main_window.resizable( 0, 0 ) # this prevents from resizing the window
 
-frame = tkinter.Frame( main_window, width = WIDTH, height = 50, bd = 0, bg = "grey" )
-frame.pack()
+main_window.grid_rowconfigure(1, weight=1)
+main_window.grid_columnconfigure(0, weight=1)
 
-tkinter.Label( frame, text = "Test Label" ).place()
-tkinter.Button( frame, text = "test button 1", command = nothing ).place()
-tkinter.Button( main_window, text = "test button 2", command = nothing ).pack()
+#frame = tkinter.Frame( main_window, pady = 3, background = "grey" )
+#tkinter.Label( frame, text = "Test Label", bg = "grey", font = ( "Courier", 20 )  ).pack( side = LEFT )
+#frame.grid(row=0, sticky="ew")
+
+
+top_button = tkinter.Button( main_window, text = "Test Label Button", background = TITLE_COLOR, font = ( "Courier", 20 ), anchor="w", relief=FLAT, takefocus = False, bd = 0, command = nothing )
+top_button.grid( row = 0, sticky="ew" )
+#top_button.pack( side = LEFT )
+#tkinter.Button( main_window, text = "test button 2", command = nothing ).pack()
 
 main_window.mainloop()

40885d51a4dad74a491a899652f2928a8070df62
Nathan Bradley <NJBradley1@buffs.wtamu.edu>
2019-01-29 12:53:16 -0600

    added a few more frames to the UI

--- UI/UI2.py
+++ UI/UI2.py
@@ -2,6 +2,7 @@
 
 from tkinter import *
 import tkinter.messagebox as tm
+import tkinter.filedialog as fd
 
 WIDTH = 350
 HEIGHT = 460
@@ -9,7 +10,7 @@ LOGGED_IN = False
 
 class Application( Tk ):
 	def __init__( self ):
-		Tk.__init__( self) 
+		super().__init__() 
 		self.title( "GeLP" )
 		#self.geometry( '{}x{}'.format( WIDTH, HEIGHT) )
 		#self.resizable( 0, 0 ) # this prevents from resizing the window
@@ -26,12 +27,13 @@ class Application( Tk ):
 
 		self._frame = new_frame
 
+
 class LoginFrame( Frame ):
 	def __init__( self, master ):
 		super().__init__( master )
 
-		self.label_username = Label( self, text = "Username" )
-		self.label_password = Label( self, text = "Password" )
+		self.label_username = Label( self, text = "Username:" )
+		self.label_password = Label( self, text = "Password:" )
 
 		self.entry_username = Entry( self )
 		self.entry_password = Entry( self, show = "*" )
@@ -41,12 +43,12 @@ class LoginFrame( Frame ):
 		self.entry_username.grid( row = 0, column = 1 )
 		self.entry_password.grid( row = 1, column = 1 )
 
-		self.logbtn = Button( self, text = "Login", command = self._login_btn_clicked )
-		self.logbtn.grid( columnspan = 2 )
+		self.login_button = Button( self, text = "Login", command = self._login_clicked )
+		self.login_button.grid( columnspan = 2 )
 
 		self.pack( anchor = "nw" )
 
-	def _login_btn_clicked( self ):
+	def _login_clicked( self ):
 		username = self.entry_username.get()
 		password = self.entry_password.get()
 		
@@ -54,7 +56,7 @@ class LoginFrame( Frame ):
 
 		global LOGGED_IN
 		LOGGED_IN = True
-		self.master.switch_frame( second_frame )
+		self.master.switch_frame( Choiceframe )
 
 		#if username == "admin" and password == "password":
 		#    tm.showinfo( "Login info", "admin accepted" )
@@ -62,14 +64,73 @@ class LoginFrame( Frame ):
 		#    tm.showerror( "Login error", "Incorrect username" )
 
 
-class second_frame( Frame ):
+class Choiceframe( Frame ):
 	def __init__( self, master ):
 		super().__init__( master )
 
-		self.text_1 = Label( self, text = "Sample Text" )
-		self.text_1.pack( anchor = "nw" )
+		self.enter_button = Button( self, text = "Enter Data", command = self._enter_clicked )
+		self.import_button = Button( self, text = "Import File", command = self._import_clicked )
+
+		self.enter_button.grid( row = 0, sticky = E )
+		self.import_button.grid( row = 0, column = 1 )
 
 		self.pack( anchor = "nw" )
+
+	def _enter_clicked( self ):
+		self.master.switch_frame( EnterDataframe )
+
+	def _import_clicked( self ):
+		file_path = fd.askopenfilename()
+		#check file type
+		#pass file to file processing
+		#pass data in file to AI algorithm
+		self.master.switch_frame( ResultFrame )
+
+
+class EnterDataframe( Frame ):
+	def __init__( self, master ):
+		super().__init__( master )
+
+		self.default_label_1 = Label( self, text = "Type in the values into each box and" )
+		self.default_label_2 = Label( self, text = "then click next data point or continue." )
+
+		self.time_entry = Entry( self )
+		self.value_entry = Entry( self )
+
+		self.next_button = Button( self, text = "Next Data Point", command = self._next_clicked )
+		self.continue_button = Button( self, text = "Continue", command = self._continue_clicked )
+
+		self.default_label_1.grid( row = 0, sticky = W, columnspan = 2 )
+		self.default_label_2.grid( row = 1, sticky = W, columnspan = 2 )
+		self.time_entry.grid( row = 2, sticky = E )
+		self.value_entry.grid( row = 2, column = 1 )
+		self.next_button.grid( row = 3, sticky = E )
+		self.continue_button.grid( row = 3, column = 1 )
+
+		self.pack( anchor = "nw" )
+
+	def _next_clicked( self ):
+		# append data to data collected
+		tm.showinfo( "Button info", "Button not implemented.\nNo action." )
+
+	def _continue_clicked( self ):
+		# append data to data collected
+		#pass data to AI algorithm
+		self.master.switch_frame( ResultFrame )
+
+
+class ResultFrame( Frame ):
+	def __init__( self, master ):
+		super().__init__( master )
+
+		self.default_label_1 = Label( self, text = "Frame not implemented." )
+		self.default_label_2 = Label( self, text = "No progression." )
+
+		self.default_label_1.pack( anchor = "nw" )
+		self.default_label_2.pack( anchor = "nw" )
+
+		self.pack( anchor = "nw" )
+
 
 class TopMenu( Menu ):
 	def __init__( self, master ):
@@ -94,7 +155,7 @@ class TopMenu( Menu ):
 		global LOGGED_IN
 
 		if( LOGGED_IN ):
-			self.master.switch_frame( second_frame )
+			self.master.switch_frame( Choiceframe )
 
 	def _Exit_selected( self ):
 		self.master.destroy()

94dc07a19701f88974dc6b0db87b522f621b6cb1
tjmoore06 <45438970+tjmoore06@users.noreply.github.com>
2019-02-28 11:47:10 -0600

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,266 +1,267 @@
 #! /usr/bin/env python
 
-import sys
-sys.path.append('/home/nb1003151/openpyxl-2.6.0')
-
 import openpyxl
+
+from tkinter import *
+import tkinter.messagebox as tm
+import tkinter.filedialog as fd
 
 import hashlib
 import binascii
 import compare
-from tkinter import *
-import tkinter.messagebox as tm
-import tkinter.filedialog as fd
 
 WIDTH = 350
 HEIGHT = 460
 LOGGED_IN = False
 
-class Application( Tk ):
-	def __init__( self ):
-		super().__init__() 
-		self.title( "GeLP" )
-		#self.geometry( '{}x{}'.format( WIDTH, HEIGHT) )
-		#self.resizable( 0, 0 ) # this prevents from resizing the window
-		self._menu = TopMenu( self )
-		self._frame = None
-		self._to_do = None
-		self.switch_frame( LoginFrame( self ) )
 
-	def switch_frame( self, new_frame ):
-		#Destroys current frame and replaces it with a new one.
-		if self._frame is not None:
-			self._frame.destroy()
+class Application(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("GeLP")
+        # self.geometry( '{}x{}'.format( WIDTH, HEIGHT) )
+        # self.resizable( 0, 0 ) # this prevents from resizing the window
+        self._menu = TopMenu(self)
+        self._frame = None
+        self._to_do = None
+        self.switch_frame(LoginFrame(self))
 
-		self._frame = new_frame
-		self.update()
+    def switch_frame(self, new_frame):
+        # Destroys current frame and replaces it with a new one.
+        if self._frame is not None:
+            self._frame.destroy()
 
-		if self._to_do:
-			do_now = self._to_do
-			self._to_do = None;		# prevents doing it again
-			do_now();
+        self._frame = new_frame
+        self.update()
 
+        if self._to_do:
+            do_now = self._to_do
+            self._to_do = None  # prevents doing it again
+            do_now()
 
-class LoginFrame( Frame ):
-	def __init__( self, master ):
-		super().__init__( master )
 
-		self.label_username = Label( self, text = "Username:" )
-		self.label_password = Label( self, text = "Password:" )
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
 
-		self.entry_username = Entry( self )
-		self.entry_password = Entry( self, show = "*" )
+        self.label_username = Label(self, text="Username:")
+        self.label_password = Label(self, text="Password:")
 
-		self.label_username.grid( row = 0, sticky = E )
-		self.label_password.grid( row = 1, sticky = E )
-		self.entry_username.grid( row = 0, column = 1 )
-		self.entry_password.grid( row = 1, column = 1 )
+        self.entry_username = Entry(self)
+        self.entry_password = Entry(self, show="*")
 
-		self.login_button = Button( self, text = "Login", command = self._login_clicked )
-		self.login_button.grid( columnspan = 2 )
+        self.label_username.grid(row=0, sticky=E)
+        self.label_password.grid(row=1, sticky=E)
+        self.entry_username.grid(row=0, column=1)
+        self.entry_password.grid(row=1, column=1)
 
-		self.pack( anchor = "nw" )
+        self.login_button = Button(self, text="Login", command=self._login_clicked)
+        self.login_button.grid(columnspan=2)
 
-	def encrypt(self, password):
-		password = binascii.a2b_qp(password)
-		en = hashlib.pbkdf2_hmac('sha1', password, b'kYt20AgK9e3MdRGivNqT', 100000)
-		return en
+        self.pack(anchor="nw")
 
-	def _login_clicked( self ):
-		username = self.entry_username.get()
-		password = self.entry_password.get()
-		
-		password = self.encrypt(password)
-		if (compare.compare(username, password)):
-			tm.showinfo( "Login info","logged in ")
-			global LOGGED_IN
-			LOGGED_IN = True
-			self.master.switch_frame(Choiceframe(self.master))
-		else:
-			tm.showinfo( "Login info", "failed." )
+    def encrypt(self, password):
+        password = binascii.a2b_qp(password)
+        en = hashlib.pbkdf2_hmac('sha1', password, b'kYt20AgK9e3MdRGivNqT', 100000)
+        return en
 
-		#if username == "admin" and password == "password":
-		#    tm.showinfo( "Login info", "admin accepted" )
-		#else:
-		#    tm.showerror( "Login error", "Incorrect username" )
+    def _login_clicked(self):
+        username = self.entry_username.get()
+        password = self.entry_password.get()
 
+        password = self.encrypt(password)
+        if (compare.compare(username, password)):
+            tm.showinfo("Login info", "logged in ")
+            global LOGGED_IN
+            LOGGED_IN = True
+            self.master.switch_frame(Choiceframe(self.master))
+        else:
+            tm.showinfo("Login info", "failed.")
 
-class Choiceframe( Frame ):
-	def __init__( self, master ):
-		super().__init__( master )
+# if username == "admin" and password == "password":
+#    tm.showinfo( "Login info", "admin accepted" )
+# else:
+#    tm.showerror( "Login error", "Incorrect username" )
 
-		self.enter_button = Button( self, text = "Enter Data", command = self._enter_clicked )
-		self.import_button = Button( self, text = "Import File", command = self._import_clicked )
 
-		self.enter_button.grid( row = 0, sticky = E )
-		self.import_button.grid( row = 0, column = 1 )
+class Choiceframe(Frame):
+    def __init__(self, master):
+        super().__init__(master)
 
-		self.pack( anchor = "nw" )
+        self.enter_button = Button(self, text="Enter Data", command=self._enter_clicked)
+        self.import_button = Button(self, text="Import File", command=self._import_clicked)
 
-	def _enter_clicked( self ):
-		self.master.switch_frame( EnterDataframe( self.master ) )
+        self.enter_button.grid(row=0, sticky=E)
+        self.import_button.grid(row=0, column=1)
 
-	def _import_clicked( self ):
-		file_path = fd.askopenfilename()
+        self.pack(anchor="nw")
 
-		if file_path: # if a file was selected
-			try:
-				# Open file and load workbook
-				workbook = openpyxl.load_workbook( file_path, read_only = True )
+    def _enter_clicked(self):
+        self.master.switch_frame(EnterDataframe(self.master))
 
-			except openpyxl.utils.exceptions.InvalidFileException:
-				tm.showerror( "File error", "Unable to open file as XLSX file" )
-				return
+    def _import_clicked(self):
+        file_path = fd.askopenfilename()
 
-			## pass workbook to loading frame
-			self.master.switch_frame( Loadingframe( self.master, workbook ) )
+        if file_path:  # if a file was selected
+            try:
+                # Open file and load workbook
+                workbook = openpyxl.load_workbook(file_path, read_only=True)
 
+            except openpyxl.utils.exceptions.InvalidFileException:
+                tm.showerror("File error", "Unable to open file as XLSX file")
+                return
 
-class Loadingframe( Frame ):
-	def __init__( self, master, input_workbook ):
-		super().__init__( master )
+            ## pass workbook to loading frame
+            self.master.switch_frame(Loadingframe(self.master, workbook))
 
-		self._workbook = input_workbook
 
-		self.default_label = Label( self, text = "File loading." )
-		self.default_label.pack( anchor = "nw" )
+class Loadingframe(Frame):
+    def __init__(self, master, input_workbook):
+        super().__init__(master)
 
-		self.pack( anchor = "nw" )
+        self._workbook = input_workbook
 
-		master._to_do = self.process_workbook
+        self.default_label = Label(self, text="File loading.")
+        self.default_label.pack(anchor="nw")
 
-	def process_workbook( self ):
+        self.pack(anchor="nw")
 
-		sheet = self._workbook.active
+        master._to_do = self.process_workbook
 
-		intermediate_array = [ column for column in sheet.iter_rows( min_row = 1, max_col = sheet.max_column, max_row = sheet.max_row, values_only = True ) ]
+    def process_workbook(self):
 
-		data_array = []
+        sheet = self._workbook.active
 
-		for column in range( sheet.max_column ):
-			line = [ row[ column ] for row in intermediate_array if isinstance( row[ column ], float ) ]
+        intermediate_array = [column for column in
+                              sheet.iter_rows(min_row=1, max_col=sheet.max_column, max_row=sheet.max_row,
+                                              values_only=True)]
 
-			if line:
-				data_array.append( line )
+        data_array = []
 
-		self.master.switch_frame( ResultFrame( self.master, data_array ) )
+        for column in range(sheet.max_column):
+            line = [row[column] for row in intermediate_array if isinstance(row[column], float)]
 
+            if line:
+                data_array.append(line)
 
-class EnterDataframe( Frame ):
-	def __init__( self, master ):
-		super().__init__( master )
+        self.master.switch_frame(ResultFrame(self.master, data_array))
 
-		self.default_label_1 = Label( self, text = "Type in the values into each box and" )
-		self.default_label_2 = Label( self, text = "then click Next Data Point or Continue." )
 
-		self.time_entry = Entry( self )
-		self.value_entry = Entry( self )
+class EnterDataframe(Frame):
+    def __init__(self, master):
+        super().__init__(master)
 
-		self.next_button = Button( self, text = "Next Data Point", command = self._next_clicked )
-		self.continue_button = Button( self, text = "Continue", command = self._continue_clicked )
+        self.default_label_1 = Label(self, text="Type in the values into each box and")
+        self.default_label_2 = Label(self, text="then click Next Data Point or Continue.")
 
-		self.default_label_1.grid( row = 0, sticky = W, columnspan = 2 )
-		self.default_label_2.grid( row = 1, sticky = W, columnspan = 2 )
-		self.time_entry.grid( row = 2, sticky = E )
-		self.value_entry.grid( row = 2, column = 1 )
-		self.next_button.grid( row = 3, sticky = E )
-		self.continue_button.grid( row = 3, column = 1 )
+        self.time_entry = Entry(self)
+        self.value_entry = Entry(self)
 
-		self.data = {}
+        self.next_button = Button(self, text="Next Data Point", command=self._next_clicked)
+        self.continue_button = Button(self, text="Continue", command=self._continue_clicked)
 
-		self.pack( anchor = "nw" )
+        self.default_label_1.grid(row=0, sticky=W, columnspan=2)
+        self.default_label_2.grid(row=1, sticky=W, columnspan=2)
+        self.time_entry.grid(row=2, sticky=E)
+        self.value_entry.grid(row=2, column=1)
+        self.next_button.grid(row=3, sticky=E)
+        self.continue_button.grid(row=3, column=1)
 
-	def _next_clicked( self ):
-		time_text = self.time_entry.get()
-		value_text = self.value_entry.get()
+        self.data = {}
 
-		if time_text or value_text:		# if the entries are not empty
-			try:
-				time_ = float( time_text )
-				value_ = float( value_text )
-			except ValueError:
-				tm.showinfo( "Input Error", "Invalid entry" )
-				return
+        self.pack(anchor="nw")
 
-			# append data to data collected
-			self.data.update( { time_: value_ } )
+    def _next_clicked(self):
+        time_text = self.time_entry.get()
+        value_text = self.value_entry.get()
 
-			# clear entrys
-			self.time_entry.delete( 0, END )
-			self.value_entry.delete( 0, END )
+        if time_text or value_text:  # if the entries are not empty
+            try:
+                time_ = float(time_text)
+                value_ = float(value_text)
+            except ValueError:
+                tm.showinfo("Input Error", "Invalid entry")
+                return
 
-	def _continue_clicked( self ):
-		time_text = self.time_entry.get()
-		value_text = self.value_entry.get()
+            # append data to data collected
+            self.data.update({time_: value_})
 
-		if time_text or value_text:		# if the entries are not empty
-			try:
-				time_ = float( time_text )
-				value_ = float( value_text )
-			except ValueError:
-				tm.showinfo( "Input Error", "Invalid entry" )
-				return
+            # clear entrys
+            self.time_entry.delete(0, END)
+            self.value_entry.delete(0, END)
 
-			# append data to data collected
-			self.data.update( { time_: value_ } )
+    def _continue_clicked(self):
+        time_text = self.time_entry.get()
+        value_text = self.value_entry.get()
 
-		keys = list( self.data.keys() )
-		keys.sort()
+        if time_text or value_text:  # if the entries are not empty
+            try:
+                time_ = float(time_text)
+                value_ = float(value_text)
+            except ValueError:
+                tm.showinfo("Input Error", "Invalid entry")
+                return
 
-		# organize time and value into two ordered lists
-		data_array = [ keys, [ self.data[ index ] for index in keys ] ]
+            # append data to data collected
+            self.data.update({time_: value_})
 
-		## pass data to AI algorithm
-		self.master.switch_frame( ResultFrame( self.master, data_array ) )
+        keys = list(self.data.keys())
+        keys.sort()
 
+        # organize time and value into two ordered lists
+        data_array = [keys, [self.data[index] for index in keys]]
 
-class ResultFrame( Frame ):
-	def __init__( self, master, input_data ):
-		super().__init__( master )
+        ## pass data to AI algorithm
+        self.master.switch_frame(ResultFrame(self.master, data_array))
 
-		for column_index, column in enumerate( input_data ):
-			for row_index, value in enumerate( column ):
-				default_label = Label( self, text = value )
-				default_label.grid( row = row_index, column = column_index )
 
-		#self.default_label_1.grid( row = 0, sticky = E )
+class ResultFrame(Frame):
+    def __init__(self, master, input_data):
+        super().__init__(master)
 
-		self.pack( anchor = "nw" )
+        for column_index, column in enumerate(input_data):
+            for row_index, value in enumerate(column):
+                default_label = Label(self, text=value)
+                default_label.grid(row=row_index, column=column_index)
 
+        # self.default_label_1.grid( row = 0, sticky = E )
 
-class TopMenu( Menu ):
-	def __init__( self, master ):
-		super().__init__( master )
+        self.pack(anchor="nw")
 
-		master.configure( menu = self )
 
-		self.sub_menu = Menu( master, tearoff = 0 )
-		self.add_cascade( menu = self.sub_menu, label = "File" )
-		self.sub_menu.add_command( label = "Log out", command = self._Log_out_selected )
-		self.sub_menu.add_command( label = "Reset", command = self._Reset_selected )
-		self.sub_menu.add_command( label = "Exit", command = self._Exit_selected )
+class TopMenu(Menu):
+    def __init__(self, master):
+        super().__init__(master)
 
-	def _Log_out_selected(self):
-		global LOGGED_IN
+        master.configure(menu=self)
 
-		if( LOGGED_IN ):
-			LOGGED_IN = False
-			self.master.switch_frame( LoginFrame( self.master ) )
+        self.sub_menu = Menu(master, tearoff=0)
+        self.add_cascade(menu=self.sub_menu, label="File")
+        self.sub_menu.add_command(label="Log out", command=self._Log_out_selected)
+        self.sub_menu.add_command(label="Reset", command=self._Reset_selected)
+        self.sub_menu.add_command(label="Exit", command=self._Exit_selected)
 
-	def _Reset_selected( self ):
-		global LOGGED_IN
+    def _Log_out_selected(self):
+        global LOGGED_IN
 
-		if( LOGGED_IN ):
-			self.master.switch_frame( Choiceframe( self.master ) )
+        if (LOGGED_IN):
+            LOGGED_IN = False
+            self.master.switch_frame(LoginFrame(self.master))
 
-	def _Exit_selected( self ):
-		# log out for consistancy
-		global LOGGED_IN
-		LOGGED_IN = False
+    def _Reset_selected(self):
+        global LOGGED_IN
 
-		self.master.destroy()
-		self.master = None
-		# this exits the program
+        if (LOGGED_IN):
+            self.master.switch_frame(Choiceframe(self.master))
+
+    def _Exit_selected(self):
+        # log out for consistancy
+        global LOGGED_IN
+        LOGGED_IN = False
+
+        self.master.destroy()
+        self.master = None
+# this exits the program
 
 
 if __name__ == "__main__":

18502105d4ce65eb10f831a99c8088276ea5909d
tjmoore06 <45438970+tjmoore06@users.noreply.github.com>
2019-02-28 10:12:41 -0600

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,20 +1,12 @@
 #! /usr/bin/env python
 import binascii
-import sqlite3
-import os
-
-"""
-This code is to check if the username and password are in the database
-author: Timothy Moore
-Version: 02/07/2019
-"""
 def compare(username, password):
-  connection = sqlite3.connect(os.path.join(os.getcwd(), 'DataBase', 'Users.db'))
-  cursor = connection.cursor() #The past 2 lines connect to database
-  cursor.execute("SELECT password FROM user WHERE user.id = \"" +username+ "\";") #gets password from username
-  p = str(cursor.fetchone())
-  if("('"+password+"',)" == p):#compares password entered to the users password
-    return True
+  f = open('users', "r")#find the user information
+  u = f.readline().rstrip()
+  p = f.readline().rstrip()
+  f.close
+  if(username==u):#compair user name
+    return binascii.hexlify(password).decode("utf-8") == p#compair password
   else:
     return False
-
+  
